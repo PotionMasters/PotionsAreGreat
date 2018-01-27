7fea144b484d40800a4e63b36d105ef4f16ff982
Robert Wild <wildrobertg@gmail.com>
2018-01-27 12:56:08 -0500

    Network stuff - with bug, update gitignore

---
+++ Potions V2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : MonoBehaviour
+{
+    public SeedManager seeder;
+
+    public Player[] Players { get; private set; }
+    public Player BookPlayer { get; private set; }
+    public Player PotionPlayer { get; private set; }
+    public Player LocalHuman { get; private set; }
+
+    public bool IsPlaying { get; private set; }
+    public bool PlayersReady { get; private set; }
+    private System.Action onPlayersReady;
+
+
+    public void RegisterPlayer(Player player)
+    {
+        Players[player.id] = player;
+        if (player.Role == PlayerRole.Book)
+        {
+            BookPlayer = player;
+            Debug.Log("Registered book player (player " + player.id + ")");
+        }
+        else
+        {
+            PotionPlayer = player;
+            Debug.Log("Registered potion player (player " + player.id + ")");
+        }
+
+        if (BookPlayer != null && PotionPlayer != null)
+        {
+            OnAllPlayersRegistered();
+        }
+    }
+    public void DoOncePlayersReady(System.Action action)
+    {
+        Debug.Log("All players Ready");
+        if (PlayersReady) action();
+        onPlayersReady += action;
+    }
+
+    private void Awake()
+    {
+        Players = new Player[2];
+        IsPlaying = false;
+
+        //seeder.onSeedSet += (int seed) => // setup
+    }
+    private void OnAllPlayersRegistered()
+    {
+        // Set Ready
+        PlayersReady = true;
+        IsPlaying = true;
+
+        // Event
+        if (onPlayersReady != null)
+            onPlayersReady();
+    }
+}
---
+++ Potions V2/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public enum PlayerRole { Potion, Book }
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar] public short id;
+    private GameManager gm;
+    public PlayerRole Role { get; private set; }
+
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+    private void Start()
+    {
+        Role = id == 0 ? PlayerRole.Book : PlayerRole.Potion;
+
+        gm.RegisterPlayer(this);
+    }
+
+    public bool IsLocalHuman()
+    {
+        return isLocalPlayer;
+    }
+}
