ddc2333e82deabaa3d666feaeb0b41a93a549d03
gwlenglish <gwlenglish@gmail.com>
2018-01-27 11:14:46 -0500

    first scripts

---
+++ Potions V2/Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    [Tooltip("Spacebar initiates the main theme ")]
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
+
+    void Start()
+    {
+        AudioManager.instance.PlayMusic(mainTheme, 3);
+      
+    }
+
+    void Update()
+    {
+       
+    
+
+    }
+
+   
+}
---
+++ Potions V2/Assets/Scripts/Util/SetResolution.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(1280, 720, true);
+    }
+	
+
+}
